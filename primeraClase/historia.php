78253c5baabd7b3ced3b933234fd2cdebe2165c5
Angel Muñoz <munozbenavidesangel@gmail.com>
2025-12-14 01:55:40 -0500

    1er maquetado historia clinica

--- primeraClase/historia.php
+++ primeraClase/historia.php
@@ -1,3 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historia Clinica</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="encabezado">
+        <form action="get"></form>
+    </div>
+    <div class="container">
+        <div id="datospaciente">
+            <h1>Datos paciente</h1>
+            <div class="doblecolumna">
+                <div id="columnas">
+                    <p>Nombre: <span>nombre</span></p>
+                </div>
+                <div id="columnas">
+                    <p>Doc: <span>C.C. 1089245125</span></p>
+                </div>
 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<?php
+$SUPA_URL ='https://xxrcprscclooumnzonbu.supabase.co';
+$SUPA_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4cmNwcnNjY2xvb3VtbnpvbmJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0OTk2OTUsImV4cCI6MjA4MTA3NTY5NX0.Hu2gQ1y22T7lcy15RJJtBQ5L01gtMYFyPZj9z39Havg';
 ?>
